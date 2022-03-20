8563415940f9d561ce14b21bb76faec60a6b4f33
Kirill Shalaev <48245126+shalaevk@users.noreply.github.com>
2022-03-20 11:13:09 +0200

    Changed phone

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -16,8 +16,8 @@ export const Footer = (props) => {
 				.webstuff
 			</div>
 			<div className={cls.number}>
-				<a className={cls.phone} href="tel:+380634247864">
-					+38 (066) 424-78-64
+				<a className={cls.phone} href="tel:+380508716557">
+					+38 (050) 871-65-57
 					<img src={phone} />
 				</a>
 				<a className={cls.social} href="viber://chat?number=%2B380508716557">
