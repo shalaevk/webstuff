2ce1f510376cc7ecd00f5338eceae49573f69c1e
Kirill Shalaev <48245126+shalaevk@users.noreply.github.com>
2022-05-24 13:12:58 +0300

    1

--- src/layout/Form.jsx
+++ src/layout/Form.jsx
@@ -100,7 +100,7 @@ export default class Form extends React.Component {
 							onSubmit={this.handleSubmit}
 						>
 							{/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-							<input type="hidden" name="Zakaz" value="contact" />
+							<input type="hidden" name="Contact-form" value="contact" />
 							<div hidden>
 								<label>
 									Don’t fill this out:{" "}
